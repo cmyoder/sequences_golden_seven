6757f70c042bfe7f5d1a6c1a66ee0d0f7c7675ed
cmyoder <cmyoder@uchicago.edu>
2017-02-01 17:14:49 -0600

    id pages

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -3,13 +3,22 @@ class GuessesController < ApplicationController
   def index
     @list_of_guesses = Guess.all
 
-
     render("index.html.erb")
   end
 
-  def id
-    @id = params["id"]
+  def show
+    @id = params[:id]
 
-    render("id.html.erb")
+    @first_number = Guess.find(@id).first_number.to_i
+    @second_number = Guess.find(@id).second_number.to_i
+    @third_number = Guess.find(@id).third_number.to_i
+
+    if @first_number < @second_number && @second_number < @third_number
+      @result = "Yes!"
+    else
+      @result = "No."
+    end
+
+    render("show.html.erb")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get("/guesses", { :controller => "guesses", :action => "index"})
-  get("/guesses/:id", { :controller => "guesses", :action => "id"})
+  get("/guesses/:id", { :controller => "guesses", :action => "show"})
 end
